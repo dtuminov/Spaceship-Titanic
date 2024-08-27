8350a2fcd89c74f061c9149684be564d6b624402
Dmitry Kozlov <91003792+dtuminov@users.noreply.github.com>
2024-08-27 18:30:30 +0300

    Service is working!

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from pydantic import BaseModel
 import skops.io as sio
 import numpy as np
 from fastapi.responses import FileResponse, JSONResponse
+from skops.io import get_untrusted_types
+
 
 class Item(BaseModel):
     PassengerId: object
@@ -38,27 +40,27 @@ def covert_data(arr: list):
         arr[4] = 1
     elif arr[4] == 'PSO J318.5-22':
         arr[4] = 2
-
-    str = arr[3]
-    if str[0] == 'D':
+    st = ""
+    st = str(arr[3])
+    if st[0] == 'D':
         arr[3] = 0
-    elif str[0] == 'B':
+    elif st[0] == 'B':
         arr[3] = 1
-    elif str[0] == 'C':
+    elif st[0] == 'C':
         arr[3] = 2
-    elif str[0] == 'A':
+    elif st[0] == 'A':
         arr[3] = 3
-    elif str[0] == 'E':
+    elif st[0] == 'E':
         arr[3] = 4
-    elif str[0] == 'F':
+    elif st[0] == 'F':
         arr[3] = 5
-    elif str[0] == 'T':
+    elif st[0] == 'T':
         arr[3] = 6
-    elif str[0] == 'G':
+    elif st[0] == 'G':
         arr[3] = 7
 
     arr = np.asarray(arr)
-    arr = np.delete(arr, [0, 7, 8, 9, 10, 11])
+    arr = np.delete(arr, [0, 12])
     arr = arr.tolist()
     int_list = [int(float(i)) for i in arr]
     int_list = np.asarray(int_list)
@@ -70,8 +72,10 @@ def covert_data(arr: list):
 async def prediction(item: Item):
     arr = [item.PassengerId,item.HomePlanet,item.CryoSleep,item.Cabin,
            item.Destination, item.Age, item.VIP, item.RoomService,
-           item.FoodCourt, item.ShoppingMall, item.Spa, item.VRDeck]
-    model = sio.load('my-model.skops')
+           item.FoodCourt, item.ShoppingMall, item.Spa, item.VRDeck, item.Name]
+    unknown_types = get_untrusted_types(file="my-model.skops")
+    model = sio.load('my-model.skops',trusted=unknown_types)
+
     prediction = model.predict(covert_data(arr))
     prediction_proba = model.predict_proba(covert_data(arr))
     prediction = prediction.tolist()
